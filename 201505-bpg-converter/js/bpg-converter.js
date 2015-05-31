cc1d438fbd6fb546429588d690d84891854669c2
Kinuko Yasuda <kinuko@chromium.org>
2015-05-31 18:18:44 +0900

    BPG converter test

---
+++ 201505-bpg-converter/js/bpg-converter.js
@@ -0,0 +1,14 @@
+onfetch = function(evt){
+    var url = evt.request.url;
+    if(/\.bpg\.png$/.test(url)){
+        evt.respondWith(
+            fetch(url.substr(0,url.length - 4)).then(function(res){
+                return res.arrayBuffer();
+            }).then(function(buffer) {
+                var converter = new Converter(window.BPGDecoder, window.PNGencoder);
+                var png = converter.convert(buffer);
+                return new Response(png, { headers: {"Content-Type": "image/png"} });
+            })
+        );
+    }
+};
